9557fd5b259edd6bafcb6d5fe966caae1c37cb9a
robson120-s <shroba391@gmail.com>
2025-10-20 20:18:50 +0300

    simple folder structure setup

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <button className ='btn btn-primary'>
+            Create Blog
+        </button>
+    </div>
+  )
+}
+
+export default Header
